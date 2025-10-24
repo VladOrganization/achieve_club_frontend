2913cc4adc019dec6ea580291530931c582c7d7e
Kirillfty <kirillhaiduk4@gmail.com>
2025-10-24 22:04:22 +0300

    fix

---
+++ src/components/codeModal.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div class="card flex justify-center" id="position">
+        <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }">
+           <p>code:1234</p>
+        </Dialog>
+    </div>
+</template>
+
+<script setup>
+const visible = defineModel('visible');
+</script>
+
+
+ 
